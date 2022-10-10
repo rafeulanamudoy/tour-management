dbbeac4818705d0b9f37428a23a7d0420f27179e
rafeulanamudoy <rafeuludoy@gmail.com>
2022-10-10 12:35:22 +0600

    server.js file changed

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const colors = require("colors");
-require('dotenv').config();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const source = process.env.DATABASE_LOCAL;
 mongoose.connect(source, { useNewUrlParser: true })
     .then(() => {
 
-        console.log("database connect succesfully".green)
+        console.log("database connect succesfully")
+
 
     })
 const port = process.env.PORT || 5000;
